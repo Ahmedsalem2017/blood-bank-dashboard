787fabbaf028826037bbfaae6f10e28ef3220d32
FaHaD <fahad.a.ict@gmail.com>
2018-03-18 03:54:49 +0300

    Add components (home,navbar,offcanvas,footer) and Translate the page to Arabic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RightOffcanvasComponent } from './components/right-offcanvas/right-offcanvas.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    RightOffcanvasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

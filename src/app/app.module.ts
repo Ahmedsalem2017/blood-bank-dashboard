6bcd475d9dc5beb447f0e2cbee739a09b033da03
Ahmed Salem <Ahmedsalem2008@gmail.com>
2018-03-20 04:44:15 +0300

    1. orgnize template parts 2. add routes 3. generate 404 component for 404 page 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/components/footer/footer.component.html 	modified:   src/app/components/nav-bar/nav-bar.component.html 	modified:   src/app/components/right-offcanvas/right-offcanvas.component.html 	new file:   src/app/components/wronge-way/wronge-way.component.css 	new file:   src/app/components/wronge-way/wronge-way.component.html 	new file:   src/app/components/wronge-way/wronge-way.component.spec.ts 	new file:   src/app/components/wronge-way/wronge-way.component.ts 	modified:   src/app/pages/feedback/feedback.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,67 @@
+import { WrongeWayComponent } from './components/wronge-way/wronge-way.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RightOffcanvasComponent } from './components/right-offcanvas/right-offcanvas.component';
 import { FooterComponent } from './components/footer/footer.component';
-=======
 import { LoginComponent } from './pages/login/login.component';
 import { DonorsComponent } from './pages/donors/donors.component';
 import { RequestsComponent } from './pages/requests/requests.component';
 import { FeedbackComponent } from './pages/feedback/feedback.component';
->>>>>>> d1ce60663a45eb9e4865fbed8ee87b98a2269685
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: FeedbackComponent
+  },
+  {
+    path: 'home',
+    component: FeedbackComponent
+  },
+{
+  path: 'donors',
+  component: DonorsComponent
+},
+{
+  path: 'requests',
+  component: RequestsComponent
+},
+{
+  path: 'feedback',
+  component: FeedbackComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: '**',
+  component: WrongeWayComponent
+}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HomeComponent,
     NavBarComponent,
     RightOffcanvasComponent,
-    FooterComponent
-=======
+    FooterComponent,
     LoginComponent,
     DonorsComponent,
     RequestsComponent,
-    FeedbackComponent
->>>>>>> d1ce60663a45eb9e4865fbed8ee87b98a2269685
+    FeedbackComponent,
+    WrongeWayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

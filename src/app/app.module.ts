839db18f4df67c790fe9a5880554d03da8df5eef
FaHaD <fahad.a.ict@gmail.com>
2018-05-01 18:43:02 +0300

    Delete DB Privacy statement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,14 +35,7 @@ import { LoginGuardService } from './services/login-guard.service';
 import { FeedarchivedComponent } from './components/feedarchived/feedarchived.component';
 
 
-var config = {
-  apiKey: "AIzaSyDmo6AiE_vJnpP4RJwwRHEGRKK9_Hq-yrY",
-  authDomain: "iq-bloodbank.firebaseapp.com",
-  databaseURL: "https://iq-bloodbank.firebaseio.com",
-  projectId: "iq-bloodbank",
-  storageBucket: "iq-bloodbank.appspot.com",
-  messagingSenderId: "349200844324"
-};
+
 
 
 const appRoutes: Routes = [
